b2610bdc34f8d5a3683e8659190dc396ba219ad4
chiranjitb20 <chiranjitbasak18@yahoo.com>
2015-11-19 15:42:17 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+public class test {
+
+  public static void main(String[] args) 
+  {
+    int n=10,first=0,second=1,next,i;
+    System.out.println("Printing first "+n+" numbers in Fibonacci Series \n");
+    for(i=0;i<n;i++)
+    {
+      if(i<=1)
+      next=i;
+      else
+      {
+        next = first + second;
+        first = second;
+        second = next;
+      }
+    System.out.print(next + "\t");
+    }
+  }
+}
