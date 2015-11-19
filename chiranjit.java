de75a8dfd0ccf7540e9cf65f9dd09bd723352d14
chiranjitb20 <chiranjitbasak18@yahoo.com>
2015-11-19 11:21:45 +0530

    Update chiranjit.java

--- chiranjit.java
+++ chiranjit.java
@@ -1,11 +1,11 @@
 public class chiranjit {
 
    public static void main(String[] args) {
-       int n = 20;
+       int n = 10;
        int result = 1;
        for (int i = 1; i <= n; i++) {
            result = result * i;
        }
-       System.out.println("The factorial of 20 is " + result);
+       System.out.println("The factorial of 10 is " + result);
    }
 }
